b9821248ace592312d280f638e09bcb94f998913
Mkawera1 <kaweramk@gmail.com>
2019-09-04 18:05:38 -0400

    First Scanner Exercise

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Employee {
+    public static void main(String[] args) {
+        String firstInitial="P";
+        String lastName="Sherman";
+        int houseNumber=42;
+        String streetName="Wallaby";
+        String streetType="Way";
+        String city="Sydney";
+        String khk;
+
+        Scanner keybord= new Scanner(System.in);
+        System.out.print(firstInitial + " "+ lastName + " " + houseNumber + " ");
+        System.out.println(streetName + " " + streetType + " " + city + " ");
+
+    }
+}
